6f42b120efb597d0a082e809bab9e87778005c09
Colm <colm@stratait.ie>
2025-02-07 12:07:34 +0000

    update to orderpads popover

---
+++ iarcos/iArcos/iPad/ViewControllers/Utilities/UINavigationControllerStyleHelper.m
@@ -0,0 +1,9 @@
+//
+//  UINavigationControllerStyleHelper.m
+//  iArcos
+//
+//  Created by Colm Kenna on 06/02/2025.
+//  Copyright © 2025 Strata IT Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
